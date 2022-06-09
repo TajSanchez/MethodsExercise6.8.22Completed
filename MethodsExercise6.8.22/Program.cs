2cdfe8a476ff7f4412a1bda7ea6bb8fbdff9352d
Taj <tajmarie92@gmail.com>
2022-06-08 20:42:59 -0500

    Add project files.

---
+++ MethodsExercise6.8.22/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MethodsExercise6._8._22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SayHi("Mike", 33);
+            SayHi("John", 56);
+            SayHi("Tom", 12);
+            Console.ReadLine();
+        }
+
+        static void SayHi(string name, int age)
+        {
+            Console.WriteLine("Hello " + name + " you are " + age );
+        }
+    }
+}
